10117e33c8265d74a8d2138a887c32bf85aa29cb
Vladislav <vladislav.betivu@ellation.com>
2021-06-18 17:06:09 +0300

    Create basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate gio;
+extern crate gtk;
+
+// To import all needed traits.
+//use gio::prelude::*;
+use gtk::{prelude::*, Window, WindowPosition, WindowType};
+
 fn main() {
-    println!("Hello, world!");
+    gtk::init().unwrap();
+
+    init_window();
+
+    gtk::main();
+}
+
+fn init_window() {
+    let popup = Window::new(WindowType::Popup);
+
+    popup.set_size_request(500, 700);
+    popup.set_position(WindowPosition::Center);
+
+    popup.show_all();
 }
